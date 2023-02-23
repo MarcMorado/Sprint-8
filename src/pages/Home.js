6c063441529a8eb0bcefc2f461b0075076030334
MarcMorado <66840693+MarcMorado@users.noreply.github.com>
2023-02-23 09:48:28 +0100

    home fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,6 @@ export default function Home() {
         <h1 className="home-land">
           all of your star wars favourites now streaming on disney +
         </h1>
-        {/* <Slideshow /> */}
-        {/* https://www.youtube.com/watch?v=q00ldTrywLU */}
         <div className="img-slide">
           <Slideshow controles={true}>
             <Slide>
